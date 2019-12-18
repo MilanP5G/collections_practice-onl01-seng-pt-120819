5dfde46a56899c9f2be23e881c376131cce4dcb9
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-18 15:38:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,6 @@ def sort_array_char_count(array)
   array.sort_by {|x| x.length}
 end ]
 
-def swap_elements(a, b)
-  array[1], array[2] = array[2], array[1]
+def swap_elements
+  [1], [2] = [2], [1]
 end 
