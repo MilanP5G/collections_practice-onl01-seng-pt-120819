8045bfecf8a2098f1b2b6300bb4ff8e8c683d596
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-18 16:23:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,8 +28,8 @@ def kesha_maker(array)
 end 
 
 def find_a(array)
-  new_array = []
-  array.include? {|word| new_array << word.start_with?("a")}
+  
+  array.select {|word| word.start_with?("a")}
   
 end
 
